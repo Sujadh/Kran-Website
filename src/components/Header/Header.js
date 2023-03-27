80bf26f0c866a29fe95000f1481e8fafacc11d3a
sujadh <sujadh@kran.co.in>
2023-03-27 12:43:25 +0530

    header logo navigate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,20 @@ import "./Header.css";
 import { Col, Row } from "react-bootstrap";
 import kran from "../../asset/kran.png";
 import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
+
+  const navigate = useNavigate();
+  const gotoHome = () => navigate("/");
+
   return (
     <div className="header">
       <Row>
         <Col md={0} xl={2}></Col>
         <Col xs={12} md={4} xl={4} style={{ textAlign: "start" }}>
-          <img src={kran} alt="kranLogo" />
+          <img src={kran} alt="kranLogo" onClick={gotoHome} className="kranLogo"/>
         </Col>
         <Col xs={12} md={4} xl={4} style={{ textAlign: "end" }}>
           <Navbar />
